e6417e8abe530a3331726ae76753c13ab278b710
Lucas C. Pires <lcpires7.sophia@gmail.com>
2024-04-24 12:25:40 -0300

    Update

--- src/app/christianityDataList.js
+++ src/app/christianityDataList.js
@@ -3,18 +3,21 @@ const articles = [
         title: "O que é o cristianismo",
         content: "",
         category: "christianity",
+        urlImg: 'cruzCity.jpg',
         tags:  ""
     },
     {
         title: "Sobre o batismo",
         content: "",
         category: "christianity",
+        urlImg: 'batismo.jpg',
         tags: " "
     },
     {
         title: "Sobre a santa ceia",
         content: "",
         category: "christianity",
+        urlImg: 'batismo.jpg',
         tags: " "
     }
 ]
--- src/features/Artigos/Artigos.jsx
+++ src/features/Artigos/Artigos.jsx
@@ -6,17 +6,34 @@ import '../../assets/css/Articles.css'
 
 const Article = () => {
     const category = useSelector((state) => state.currentCategoryActiveSlice)
-    
+
+
+    // function getImageUrl(name) {
+    //     const src = new URL(`./img/${name}.jpg`, import.meta.url).href
+    //     return src
+    // }
+
     return (
         <>
             <Header/>
             <main>
                 <section>
-                    <ul>
-                        {articles.map(item =>            
-                                    <li><a><img src="/img/cruzCity.jpg" /><h3>{item.title}</h3></a></li>
-                                   
-                        )}
+                    <ul key='teste'>
+                        {articles.map(item => {  
+                            const imgUrl = new URL( import.meta.url).href
+                            const imgUrl2 = new URL('/img/cruzCity.jpg', import.meta.url).href
+                            // alert(imgUrl)
+                            return (
+                                <>   
+                                    <li key={item.title.split(' ')[2]}>
+                                        <a> 
+                                            <img src={`http://localhost:5174/simplesmentecristianismo/img/${item.urlImg}`} />
+                                            <h3>{item.title}</h3>
+                                        </a>
+                                    </li>
+                                </>       
+                            )                           
+                        })}
                     </ul>
                 </section>
             </main>
